9894d80b0fff1784f30b7cf062a2ab5c2e393213
Daniel Rodríguez Pérez <daniel.rodriguez.perez@gmail.com>
2016-01-15 23:48:22 +0100

    Initial Commit

---
+++ openlibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openlibrary
+//
+//  Created by Daniel Rodríguez Pérez on 15/1/16.
+//  Copyright © 2016 Daniel Rodríguez Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
